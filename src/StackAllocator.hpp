93f88e405c93f28eaf137c0db4b06b09a9c94aee
Grigoryants Sergey <isergey.am@gmail.com>
2018-04-26 23:54:51 +0300

    StackAllocator structures encapsulated.

--- src/StackAllocator.hpp
+++ src/StackAllocator.hpp
@@ -10,63 +10,63 @@ template <typename _Tp> static _Tp *m_alloc(_Tp x = _Tp()) {
     throw std::bad_alloc();
   return new (chunk_) _Tp(x);
 }
-struct StackBlock {
-  char *memory_;
-  StackBlock *prev_block;
-  ~StackBlock() {
-    std::free(memory_);
-    if (prev_block != nullptr)
-      prev_block->~StackBlock();
-    std::free(prev_block);
-  }
-  static constexpr size_t ALLOC_MEM_ = 100000;
-  StackBlock(size_t alloc_num = ALLOC_MEM_) : prev_block(nullptr) {
-    memory_ = static_cast<char *>(std::malloc(alloc_num));
-    if (memory_ == nullptr)
-      throw std::bad_alloc();
-  }
-};
-struct StackRealAllocator {
-  size_t alloc_num;
-  char *head_;
-  StackBlock *m_block;
-  StackRealAllocator() : alloc_num(1), head_(nullptr), m_block(nullptr) {}
-  void insert_block(size_t alloc_num = StackBlock::ALLOC_MEM_) {
-    StackBlock *prev_block = m_block;
-    m_block = new (std::malloc(sizeof(StackBlock))) StackBlock(alloc_num);
-    m_block->prev_block = prev_block;
-    head_ = m_block->memory_;
-  }
-  void *allocate(size_t num_) {
-    if (num_>StackBlock::ALLOC_MEM_) {
-      auto new_block = new (std::malloc(sizeof(StackBlock))) StackBlock(num_);
-      new_block->prev_block = m_block->prev_block;
-      m_block->prev_block = new_block;
-      return new_block->memory_;
-    }
-    if (m_block == nullptr) {
-      m_block = new (std::malloc(sizeof(StackBlock))) StackBlock();
-      head_ = m_block->memory_;
-    }
-    if (head_ + num_ > m_block->memory_ + StackBlock::ALLOC_MEM_) {
-      insert_block();
-    }
-    char *ans = head_;
-    head_ += num_;
-    return static_cast<void *>(ans);
-  }
-  ~StackRealAllocator() {
-    --alloc_num;
-    if (alloc_num == 0) {
-      if (m_block != nullptr)
-        m_block->~StackBlock();
-      std::free(m_block);
-    }
-  }
-};
 template <typename T> class StackAllocator;
 template <typename T> class StackAllocator {
 private:
+  struct StackBlock {
+    char *memory_;
+    StackBlock *prev_block;
+    ~StackBlock() {
+      std::free(memory_);
+      if (prev_block != nullptr)
+        prev_block->~StackBlock();
+      std::free(prev_block);
+    }
+    static constexpr size_t ALLOC_MEM_ = 100000;
+    StackBlock(size_t alloc_num = ALLOC_MEM_) : prev_block(nullptr) {
+      memory_ = static_cast<char *>(std::malloc(alloc_num));
+      if (memory_ == nullptr)
+        throw std::bad_alloc();
+    }
+  };
+  struct StackRealAllocator {
+    size_t alloc_num;
+    char *head_;
+    StackBlock *m_block;
+    StackRealAllocator() : alloc_num(1), head_(nullptr), m_block(nullptr) {}
+    void insert_block(size_t alloc_num = StackBlock::ALLOC_MEM_) {
+      StackBlock *prev_block = m_block;
+      m_block = new (std::malloc(sizeof(StackBlock))) StackBlock(alloc_num);
+      m_block->prev_block = prev_block;
+      head_ = m_block->memory_;
+    }
+    void *allocate(size_t num_) {
+      if (num_ > StackBlock::ALLOC_MEM_) {
+        auto new_block = new (std::malloc(sizeof(StackBlock))) StackBlock(num_);
+        new_block->prev_block = m_block->prev_block;
+        m_block->prev_block = new_block;
+        return new_block->memory_;
+      }
+      if (m_block == nullptr) {
+        m_block = new (std::malloc(sizeof(StackBlock))) StackBlock();
+        head_ = m_block->memory_;
+      }
+      if (head_ + num_ > m_block->memory_ + StackBlock::ALLOC_MEM_) {
+        insert_block();
+      }
+      char *ans = head_;
+      head_ += num_;
+      return static_cast<void *>(ans);
+    }
+    ~StackRealAllocator() {
+      --alloc_num;
+      if (alloc_num == 0) {
+        if (m_block != nullptr)
+          m_block->~StackBlock();
+        std::free(m_block);
+      }
+    }
+  };
   StackRealAllocator *m_alloc;
   void free_variables() {
     if (m_alloc == nullptr)
